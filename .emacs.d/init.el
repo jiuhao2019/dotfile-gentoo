9158360ee2039e0e55007e3002ffa1bb219bcd96
jiuhao <815514981@qq.com>
2024-11-20 15:33:20 +0800

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -56,10 +56,16 @@
 (setq inhibit-startup-message t)        ; 禁用启动消息
 (setq inhibit-startup-screen t)         ; 禁用启动界面
 (setq inhibit-startup-echo-area-message t) ; 禁用 echo 区的提示信息
-(setq initial-scratch-message nil)      ; 清空 *scratch* 缓冲区的默认消息
+(setq initial-message "")
+(setq-default indicate-buffer-boundaries 'none)  ; 不显示缓冲区边界信息
+(setq-default indicate-empty-lines nil)           ; 不显示空行指示
+(setq-default echo-keystrokes 0)               ; 不显示键盘输入的回显
+(setq message-log-max nil)                     ; 禁用消息缓冲区
+(setq echo-area-message-timeout 0)             ; 禁止显示回显消息
+(setq minibuffer-message-timeout 0)            ; 禁止 minibuffer 中的提示消息
+(setq ring-bell-function 'ignore)              ; 禁用铃声和提示
 (setq initial-buffer-choice t)
 (setq inhibit-default-init t)  ; 禁止加载默认初始化文件
-(setq-default message-log-max nil) ; 禁止记录启动信息到 *Messages*
 (setq garbage-collection-messages nil)  ; 禁用 GC 信息输出
 (setq-default frame-title-format "") ; 移除窗口标题中的初始化信息
 (setq gc-cons-threshold most-positive-fixnum) ;; 只在空闲时进行 GC ，最大程度避免 GC 停顿导致的卡顿
